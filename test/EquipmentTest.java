0518642f3377c6aad9f1e72224c434fa3b4a3e41
XH <23371023@buaa.edu.cn>
2024-09-16 18:54:18 +0800

    optimize code ; add juint test

---
+++ test/EquipmentTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EquipmentTest {
+
+    @Test
+    public void DurabilityTest(){
+        Equipment Sample = new Equipment(000000,"Sample",0);
+        int dur_now = Sample.getDurability();
+        int dur_last = -1;
+        for(int i=0;i<100;i++){
+            assertEquals(dur_now,dur_last+1);
+            Sample.increaseDurability();
+            dur_last=dur_now;
+            dur_now= Sample.getDurability();
+        }
+    }
+
+}
