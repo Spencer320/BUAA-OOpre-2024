b73019d35b09254ed2d060712db284a1fcf4d4ad
XH <23371023@buaa.edu.cn>
2024-09-25 00:25:04 +0800

    minmize main class

--- src/Adventurer.java
+++ src/Adventurer.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Adventurer {
+    private final Scanner scanner = new Scanner(System.in);
     private final int id;
     private final String name;
     private HashMap<Integer,Item> items;
@@ -24,25 +26,36 @@ public class Adventurer {
         this.CE = Atk + Def;
     }
 
-    public void addBottle(int id, String name, int CE,int capacity,String type) {
-        Item bottle = new Bottle(id, name, CE,capacity, type );
-        items.put(id,bottle);
+    public void addBottle() {
+        int bottleId = scanner.nextInt();
+        String name = scanner.next();
+        int capacity = scanner.nextInt();
+        String type = scanner.next();
+        int CE = scanner.nextInt();
+        Item bottle = new Bottle(bottleId, name, CE,capacity, type );
+        items.put(bottleId,bottle);
     }
 
-    public void addEquipment(int id, String name, int durability,int CE) {
-        Item equipment = new Equipment(id, name, durability, CE);
-        items.put(id,equipment);
+    public void addEquipment() {
+        int equipmentId = scanner.nextInt();
+        String name = scanner.next();
+        int durability = scanner.nextInt();
+        int CE = scanner.nextInt();
+        Item equipment = new Equipment(equipmentId, name, durability, CE);
+        items.put(equipmentId,equipment);
     }
 
-    public void increaseDurability(int id) {
-        Equipment equipment = (Equipment) items.get(id);
+    public void increaseDurability() {
+        int equipmentId = scanner.nextInt();
+        Equipment equipment = (Equipment) items.get(equipmentId);
         equipment.increaseDurability();
         System.out.println(equipment.getName() + " " + equipment.getDurability());
     }
 
-    public void deleteItem(int id) {
-        if(items.containsKey(id)){
-            Item item = items.get(id);
+    public void deleteItem() {
+        int itemId = scanner.nextInt();
+        if(items.containsKey(itemId)){
+            Item item = items.get(itemId);
             if (item instanceof Bottle){
                 int capacity = ((Bottle) item).getCapacity();
                 System.out.println(item.getType()+" "+item.getId()+capacity);
@@ -50,37 +63,37 @@ public class Adventurer {
                 int durability = ((Equipment) item).getDurability();
                 System.out.println(item.getType()+" "+item.getId()+durability);
             }
-            items.remove(id);
+            items.remove(itemId);
         }
     }
 
-    public void carryItem(int id) {
-        Item item = items.get(id);
+    public void carryItem() {
+        int itemId = scanner.nextInt();
+        Item item = items.get(itemId);
         item.isCarry = true;
     }
 
-    public boolean useBottle(int id) {
-        Item item = items.get(id);
-        if(item instanceof Bottle){
-            Bottle bottle = (Bottle) item;
-            if (bottle.isCarry == false){
-                return false;
-            } else if (bottle.getCapacity() == 0) {
-                return false;
-            } else {
-                int value=bottle.useBottle();//value equals to expected increment
-                if (bottle instanceof HPBottle){
-                    HP += value;
-                } else if (bottle instanceof AtkBottle) {
-                    Atk += value;
-                } else if (bottle instanceof DefBottle){
-                    Def += value;
-                }
-                return true;
+    public void useBottle() {
+        int bottleId = scanner.nextInt();
+        Bottle bottle = (Bottle) items.get(bottleId);
+
+        if (!bottle.isCarry){
+            System.out.println(this.id + " fail to use " + bottle.getId());
+        } else if (bottle.getCapacity() == 0) {
+            items.remove(bottleId,bottle);
+            System.out.println(this.id + this.HP +this.Atk+this.Def);
+        } else {
+            int value=bottle.useBottle();//value equals to expected increment
+            if (bottle instanceof HPBottle){
+                HP += value;
+            } else if (bottle instanceof AtkBottle) {
+                Atk += value;
+            } else if (bottle instanceof DefBottle){
+                Def += value;
             }
-        }else {
-            return false;
+            System.out.println(this.id + this.HP +this.Atk+this.Def);
         }
+
 
     }
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -16,17 +16,35 @@ public class MainClass {
             if ("1".equals(command)) {
                 createAdventurer();
             } else if ("2".equals(command)) {
-                addBottle();
+                Adventurer adventurer = getAdventurer();
+                if (adventurer != null) {
+                    adventurer.addBottle();
+                }
             } else if ("3".equals(command)) {
-                addEquipment();
+                Adventurer adventurer = getAdventurer();
+                if (adventurer != null) {
+                    adventurer.addEquipment();
+                }
             } else if ("4".equals(command)) {
-                increaseDurability();
+                Adventurer adventurer = getAdventurer();
+                if (adventurer != null) {
+                    adventurer.increaseDurability();
+                }
             } else if ("5".equals(command)) {
-                deleteItem();
+                Adventurer adventurer = getAdventurer();
+                if (adventurer != null) {
+                    adventurer.deleteItem();
+                }
             } else if ("6".equals(command)) {
-                carryItem();
+                Adventurer adventurer = getAdventurer();
+                if (adventurer != null) {
+                    adventurer.carryItem();
+                }
             } else if ("7".equals(command)) {
-                useBottle();
+                Adventurer adventurer = getAdventurer();
+                if (adventurer != null) {
+                    adventurer.useBottle();
+                }
             }
         }
     }
@@ -38,73 +56,14 @@ public class MainClass {
         adventurerArrayList.add(adventurer);
     }
 
-    static void addBottle() {
+    static Adventurer getAdventurer(){
         int adventurerId = scanner.nextInt();
-        int bottleId = scanner.nextInt();
-        String name = scanner.next();
-        int capacity = scanner.nextInt();
-        String type = scanner.next();
-        int CE = scanner.nextInt();
         for (Adventurer adventurer : adventurerArrayList) {
             if (adventurer.getId() == adventurerId) {
-                adventurer.addBottle(bottleId, name, CE, capacity, type);
+                return adventurer;
             }
         }
+        return null;
     }
 
-    static void addEquipment() {
-        int adventurerId = scanner.nextInt();
-        int equipmentId = scanner.nextInt();
-        String name = scanner.next();
-        int durability = scanner.nextInt();
-        int CE = scanner.nextInt();
-        for (Adventurer adventurer : adventurerArrayList) {
-            if (adventurer.getId() == adventurerId) {
-                adventurer.addEquipment(equipmentId, name, durability, CE);
-            }
-        }
-    }
-
-    static void increaseDurability() {
-        int adventurerId = scanner.nextInt();
-        int equipmentId = scanner.nextInt();
-        for (Adventurer adventurer : adventurerArrayList) {
-            if (adventurer.getId() == adventurerId) {
-                adventurer.increaseDurability(equipmentId);
-            }
-        }
-    }
-
-    static void deleteItem() {
-        int adventurerId = scanner.nextInt();
-        int itemId = scanner.nextInt();
-        for (Adventurer adventurer : adventurerArrayList) {
-            if (adventurer.getId() == adventurerId) {
-                adventurer.deleteItem(itemId);
-            }
-        }
-    }
-
-    static void carryItem(){
-        int adventurerId = scanner.nextInt();
-        int itemId = scanner.nextInt();
-        for (Adventurer adventurer : adventurerArrayList) {
-            if (adventurer.getId() == adventurerId) {
-                adventurer.carryItem(itemId);
-            }
-        }
-    }
-
-    static void useBottle() {
-        int adventurerId = scanner.nextInt();
-        int bottleId = scanner.nextInt();
-        for (Adventurer adventurer : adventurerArrayList) {
-            if (adventurer.getId() == adventurerId) {
-                boolean isUse = adventurer.useBottle(bottleId);
-                if(isUse == false){
-
-                }
-            }
-        }
-    }
 }
